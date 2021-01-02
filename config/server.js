e3dd60a0f75a6f3a9c4721b33ae14f375516477d
svasandani <vasandanishailesh@gmail.com>
2021-01-01 23:50:55 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '93c675151800f98df30342ebc7b7c068'),
+    },
+  },
+});
